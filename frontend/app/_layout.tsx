c61a0db1396733b18d3de24aabf0d1272c22b9b6
Juma Josephat <91407741+JosephatJuma@users.noreply.github.com>
2025-06-30 13:49:42 +0300

    Feat: Add react native paper

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,29 +1,61 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
+import { useColorScheme } from "@/hooks/useColorScheme";
+import {
+  MD3DarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+  PaperProvider,
+} from "react-native-paper";
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMonoBold: require("../assets/fonts/SpaceMono-Bold.ttf"),
+    OutFitBold: require("../assets/fonts/Outfit-Bold.ttf"),
+    OutFitRegular: require("../assets/fonts/Outfit-Regular.ttf"),
+    OutFitMedium: require("../assets/fonts/Outfit-Medium.ttf"),
+    OutFitLight: require("../assets/fonts/Outfit-Light.ttf"),
   });
 
   if (!loaded) {
     // Async font loading only occurs in development.
     return null;
   }
+  const AppLightTheme = {
+    ...PaperDefaultTheme,
+    colors: {
+      ...PaperDefaultTheme.colors,
+      primary: "#617AFA",
+    },
+  };
+  const AppDarkTheme = {
+    ...MD3DarkTheme,
+    colors: {
+      ...MD3DarkTheme.colors,
+      primary: "#617AFA",
+    },
+  };
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <PaperProvider
+      theme={colorScheme === "dark" ? AppDarkTheme : AppLightTheme}
+    >
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(home)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+      </ThemeProvider>
+    </PaperProvider>
   );
 }
