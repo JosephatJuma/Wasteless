22d8d798f508de1c9b64b4d293df488c20065838
Juma Josephat <91407741+JosephatJuma@users.noreply.github.com>
2025-06-30 23:29:53 +0300

    fix: update auth layout styles

--- frontend/app/(auth)/_layout.tsx
+++ frontend/app/(auth)/_layout.tsx
@@ -1,8 +1,15 @@
 import { Stack } from "expo-router";
 import React from "react";
+import { useTheme } from "react-native-paper";
 const AuthenticationLayout = () => {
+  const { colors } = useTheme();
   return (
-    <Stack screenOptions={{ headerTitleStyle: { fontFamily: "OutFitBold" } }}>
+    <Stack
+      screenOptions={{
+        headerTitleStyle: { fontFamily: "OutFitBold" },
+        headerStyle: { backgroundColor: colors.background },
+      }}
+    >
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen
         name="register"
