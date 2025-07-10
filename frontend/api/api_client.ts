96aabdd95d0d07e52acea78a087678b98045130e
Juma Josephat <91407741+JosephatJuma@users.noreply.github.com>
2025-07-10 17:16:19 +0300

    Fix: Update sdk version

--- frontend/api/api_client.ts
+++ frontend/api/api_client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "http://192.168.100.101:8080",
-  timeout: 4000,
+  baseURL: "http://10.41.103.220:8080",
+  //timeout: 4000,
   // Remove the headers property here entirely or at least omit Content-Type
   // headers: {
   //   "Content-Type": "application/json",
