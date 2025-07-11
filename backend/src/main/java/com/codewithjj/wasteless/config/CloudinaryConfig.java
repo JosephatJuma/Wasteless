5f1ea91719a5827d8bb8a94360e3d8082ecc9cbf
Juma Josephat <91407741+JosephatJuma@users.noreply.github.com>
2025-07-11 19:23:09 +0300

    Chore: Updated Application for Docker render deployment

--- backend/src/main/java/com/codewithjj/wasteless/config/CloudinaryConfig.java
+++ backend/src/main/java/com/codewithjj/wasteless/config/CloudinaryConfig.java
@@ -12,10 +12,21 @@ public class CloudinaryConfig {
     public Cloudinary cloudinary() {
         String cloudinaryUrl = System.getenv("CLOUDINARY_URL");
 
+        // Use dotenv only if NOT on Render
+        if ((cloudinaryUrl == null || cloudinaryUrl.isBlank()) && System.getenv("RENDER") == null) {
+            try {
+
+                Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+                cloudinaryUrl = dotenv.get("CLOUDINARY_URL");
+            } catch (Exception e) {
+                System.out.println("Dotenv not found locally, skipping");
+            }
+        }
+
         if (cloudinaryUrl != null && !cloudinaryUrl.isBlank()) {
             return new Cloudinary(cloudinaryUrl);
         }
 
-        throw new RuntimeException("CLOUDINARY_URL not set in environment");
+        throw new RuntimeException("CLOUDINARY_URL not found in environment or .env file");
     }
 }
