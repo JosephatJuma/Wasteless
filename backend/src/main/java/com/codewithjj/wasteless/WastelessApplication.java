45be9d5a5676dee4ef83b4edce85c7f219897d3a
Juma Josephat <91407741+JosephatJuma@users.noreply.github.com>
2025-07-11 18:03:41 +0300

    Chore: Add Db connection credentials to .env for prod

--- backend/src/main/java/com/codewithjj/wasteless/WastelessApplication.java
+++ backend/src/main/java/com/codewithjj/wasteless/WastelessApplication.java
@@ -2,10 +2,20 @@ package com.codewithjj.wasteless;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class WastelessApplication {
+    Dotenv dotenv = Dotenv.load();
+
+
     public static void main(String[] args) {
+        // Load .env variables
+        Dotenv dotenv = Dotenv.load();
+
+        // Set them as system properties so Spring Boot can resolve them
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
         SpringApplication.run(WastelessApplication.class, args);
     }
 
