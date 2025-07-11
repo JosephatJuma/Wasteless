3b2ed247bd4b557c0eb80d66ead995825e6cd688
Juma Josephat <91407741+JosephatJuma@users.noreply.github.com>
2025-07-11 13:38:49 +0300

    Feat: Add cors for web requests

--- backend/src/main/java/com/codewithjj/wasteless/items/controllers/ItemController.java
+++ backend/src/main/java/com/codewithjj/wasteless/items/controllers/ItemController.java
@@ -15,7 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-
+@CrossOrigin(origins = "*") // For all origins
 @RestController
 @RequestMapping("items")
 @Tag(name = "Items", description = "Operations related to items")
